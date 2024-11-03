d4514f87bf5665b142daa87ed7ad370359c2227e
Matteo Salvarezza <matteos@wolfram.com>
2024-11-03 21:33:48 +0100

    Added 'None' unit to data

--- src/Globals.js
+++ src/Globals.js
@@ -1,4 +1,17 @@
 import partsData from './PartsData.json';
+
+const noneUnit = {
+	"Name": "None",
+	"Kind": "Unit",
+	"RightArm": true,
+	"LeftArm": true,
+	"RightShoulder": true,
+	"LeftShoulder": true,
+	"Weight": 0,
+	"ENLoad": 0
+}
+
+partsData.unshift(noneUnit)
 partsData = partsData.map((part, idx) => Object.assign(part, {ID: idx}))
 
 export default partsData
