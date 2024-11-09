8fdde4273f5417900e9c9554ae2ddd44d32cfcd2
Matteo Salvarezza <matteos@wolfram.com>
2024-11-09 22:39:42 +0100

    Part stats header

--- src/Components/PartStats.jsx
+++ src/Components/PartStats.jsx
@@ -1,29 +1,52 @@
 import StatsRow from './StatsRow.jsx';
 
-const hidddenPartStats = ['Kind', 'RightArm', 'LeftArm', 'RightShoulder', 
-	'LeftShoulder','ID'];
+const hidddenPartStats = ['Name', 'Kind', 'Manufacturer', 'Description', 'AttackType', 
+	'WeaponType', 'ReloadType', 'AdditionalEffect', 'RightArm', 'LeftArm', 'RightBack', 'LeftBack','ID'];
 
-function filterPartStats(entries) {
-	return entries.filter(([prop, val]) => !hidddenPartStats.includes(prop));
+function filterPartStats(stats) {
+	let entries = Object.entries(stats);
+	let filteredEntries = entries.filter(([prop, val]) => !hidddenPartStats.includes(prop));
+	return Object.fromEntries(filteredEntries)
+}
+
+function toNullStats(part) {
+	return Object.fromEntries(
+		Object.entries(part).map(([k, v]) => [k, null])
+	)
+}
+
+const PartStatsHeader = ({part}) => {
+	let desc;
+
+	if(part['Kind'] === 'Unit') 
+		desc = part['Description']
+	else
+		desc = part['Kind']
+	return(
+		<>
+			<div>{desc}</div>
+			<div>{part['Name']}</div>
+			<div>{part['Manufacturer']}</div>
+			<div>{part['AttackType']}</div>
+			<div>{part['WeaponType']}</div>
+			<div>{part['ReloadType']}</div>
+			<div>{part['AdditionalEffect']}</div>
+		</>
+	)
 }
 
 const PartStats = ({previewPart, curPart, visible}) => {
 
-	const curPartStats = Object.fromEntries(
-		filterPartStats(Object.entries(curPart))
-	);
 	if(previewPart === null) {
-		let nullStats = Object.fromEntries(
-			Object.entries(curPartStats).map(([k, v]) => [k, null])
-		);
-		var [leftStats, rightStats] = [nullStats, curPartStats];
+		let nullPart = toNullStats(curPart);
+		var [leftPart, rightPart] = [nullPart, curPart];
 	}
 	else {
-		var previewStats = Object.fromEntries(
-			filterPartStats(Object.entries(previewPart))
-		);
-		var [leftStats, rightStats] = [curPartStats, previewStats];
+		var [leftPart, rightPart] = [curPart, previewPart];
 	}
+
+	const leftFiltered = filterPartStats(leftPart)
+	const rightFiltered = filterPartStats(rightPart)	
 
 	let style = {flex: '0 1 600px'}
 	if(!visible)
@@ -32,20 +55,21 @@ const PartStats = ({previewPart, curPart, visible}) => {
 	return (
 		<>
 		<div style = {style}>
-		<table>
-		<tbody>
-		{
-			Object.keys(rightStats).map(
-				name => <StatsRow 
-					name = {name}
-					left = {leftStats[name]}
-					right = {rightStats[name]} 
-					key = {name}
-				/>
-			)
-		}
-		</tbody>
-		</table>
+			<PartStatsHeader part={rightPart} />
+			<table>
+			<tbody>
+				{
+					Object.keys(rightFiltered).map(
+						name => <StatsRow 
+							name = {name}
+							left = {leftFiltered[name]}
+							right = {rightFiltered[name]} 
+							key = {name}
+						/>
+					)
+				}
+			</tbody>
+			</table>
 		</div>
 		</>
 	);
