a8db8ded2aa1deef4b92556af1ee8aa2745732aa
Matteo Salvarezza <matteos@wolfram.com>
2024-12-04 12:00:50 +0100

    Improved visual feedback

--- src/Components/TablesComponent.jsx
+++ src/Components/TablesComponent.jsx
@@ -2,18 +2,54 @@ import {useState, useReducer, useContext, useRef} from 'react';
 
 import * as glob from '../Misc/Globals.js';
 
-const cellStyle = {
-	width: '160px',
-	padding: '5px 10px 5px 10px',
-	borderLeft: '2px solid ' + glob.paletteColor(5),
-	borderRight: '2px solid ' + glob.paletteColor(5),
-	textAlign: 'center'
-};
+function getCellStyle(thick) {
+	const w = thick ? '5px' : '2px';
+	return(
+		{
+			width: '160px',
+			padding: '5px 10px 5px 10px',
+			boxSizing: 'border-box',
+			borderLeftWidth: w, borderRightWidth: w,
+			borderLeftStyle: 'solid', borderRightStyle: 'solid',
+			borderLeftColor: glob.paletteColor(5), borderRightColor: glob.paletteColor(5),
+			textAlign: 'center'			
+		}
+	)
+}
 
-const DraggableHeader = ({name, pos, changeOrdering, isHovered, dragHandler}) => {
+const longDashCharacter = '\u2012';
+const doubleArrowRight = '\u00bb';
+const doubleArrowLeft = '\u00ab';
+
+function toCellDisplay(val) {
+	if(val === undefined)
+		return longDashCharacter;
+	else if (typeof val === 'string')
+		return val.toUpperCase();
+	else
+		return val
+}
+
+function getShiftSymbolsVisibility(shiftInfo, pos) {
+	if(!shiftInfo.range)
+		return ['hidden', 'hidden'];
+	else if(pos >= shiftInfo.range[0] && pos <= shiftInfo.range[1])
+		return shiftInfo.posDirection ? ['visible', 'hidden'] : ['hidden', 'visible']
+	else
+		return ['hidden', 'hidden'];
+}
+
+const DraggableHeader = ({name, pos, previewShiftInfo, changeOrdering, dragHandler}) => {
+
+	const [isBeingDragged, setIsBeingDragged] = useState(false);
 
 	const handleDragStart = (event) => {
 		event.dataTransfer.setData(JSON.stringify(pos), pos);
+		setIsBeingDragged(true);
+	};
+
+	const handleDragEnd = (event) => {
+		setIsBeingDragged(false);
 	};
 
 	const handleDragEnter = (event) =>  {
@@ -45,51 +81,68 @@ const DraggableHeader = ({name, pos, changeOrdering, isHovered, dragHandler}) =>
 		}
 	};
 
-	const background = isHovered ? glob.paletteColor(5) : glob.paletteColor(4);
+	const [leftVisible, rightVisible] = getShiftSymbolsVisibility(previewShiftInfo, pos);
+
+	let rangeEndpoint = null;
+	if(previewShiftInfo.range) {
+		rangeEndpoint = previewShiftInfo.posDirection ? previewShiftInfo.range[1] :
+			previewShiftInfo.range[0]
+	}
+
+	const cellBaseStyle = getCellStyle(pos === rangeEndpoint);
+	const color = getHeaderColor(previewShiftInfo.range, rangeEndpoint, pos);
 
 	return (
 		<th 
 			style={
 				{
-					...cellStyle,
-					backgroundColor: background,
-					cursor: 'move',
-					border: '2px solid ' + glob.paletteColor(5)
+					...cellBaseStyle,
+					backgroundColor: color,
+					height: '50px',
+					padding: '0px',
+					borderBottom: '2px solid ' + glob.paletteColor(5),
+					opacity: isBeingDragged ? 0.5 : 1
 				}
 			}
-			draggable
-			onDragStart={handleDragStart}
-			onDragEnd={() => {}}
-			onDragEnter={handleDragEnter}
-			onDragLeave={handleDragLeave}
-			onDragOver={event => event.preventDefault()}
-			onDrop={handleDrop}
 		>
-			{glob.toDisplayString(name)}
+			<div style=
+				{{display: 'flex', justifyContent: 'center', alignItems: 'center', 
+					height: '100%'}}
+			>
+				<div 
+					style={{height: '100%', width: '10%', visibility: leftVisible, display: 'flex', alignItems:'center', justifyContent: 'center', cursor: 'default'}}
+				>
+					{doubleArrowLeft}
+				</div>
+				<div 
+					style={{height: '100%', width: '80%', display: 'flex', alignItems:'center', justifyContent: 'center', cursor: 'grab'}}
+					draggable
+					onDragStart={handleDragStart}
+					onDragEnd={handleDragEnd}
+					onDragEnter={handleDragEnter}
+					onDragLeave={handleDragLeave}
+					onDragOver={event => event.preventDefault()}
+					onDrop={handleDrop}
+				>
+					{glob.toDisplayString(name)}
+				</div>
+				<div 
+					style={{height: '100%', width: '10%', visibility: rightVisible, display: 'flex', alignItems:'center', justifyContent: 'center', cursor: 'default'}}
+				>
+					{doubleArrowRight}
+				</div>
+			</div>
 		</th>
 	);
 };
 
-const longDashCharacter = '\u2012';
-
-function toCellDisplay(val) {
-	if(val === undefined)
-		return longDashCharacter;
-	else if (typeof val === 'string')
-		return val.toUpperCase();
+function getHeaderColor(range, endpoint, pos) {
+	if(pos === endpoint)
+		return glob.paletteColor(5)
+	else if(range && pos >= range[0] && pos <= range[1])
+		return glob.paletteColor(4, 1, 1.5)
 	else
-		return val
-}
-
-const toDirectionChar = {'-1': '\u21d0', '1': '\u21d2', '0': longDashCharacter};
-
-function getDirectionChar(shiftInfo, pos) {
-	if(!shiftInfo.range)
-		return longDashCharacter;
-	else if(pos >= shiftInfo.range[0] && pos <= shiftInfo.range[1])
-		return shiftInfo.posDirection ? '\u21d0' : '\u21d2'
-	else
-		return longDashCharacter;
+		return glob.paletteColor(4)
 }
 
 const DraggableTable = ({data}) => {
@@ -117,33 +170,26 @@ const DraggableTable = ({data}) => {
 		}
 	);
 
+	let rangeEndpoint = null;
+	if(previewShiftInfo.range) {
+		rangeEndpoint = previewShiftInfo.posDirection ? previewShiftInfo.range[1] :
+			previewShiftInfo.range[0]
+	}
+
 	return (
 		<table style={{tableLayout: 'fixed', borderCollapse: 'collapse', width: 'auto', 
-			width: '100%'}}>
+			width: '100%', border: '2px solid ' + glob.paletteColor(5)}}>
 			<thead>
 			<tr>
 				{
 					columnOrder.map(
-						(name, pos) => <td style={{...cellStyle, fontSize: '20px'}}>
-							{getDirectionChar(previewShiftInfo, pos)}
-						</td>
-					)
-				}
-			</tr>
-			<tr>
-				{
-					columnOrder.map(
 						(name, pos) => <DraggableHeader
-							key={name}
 							name={name}
 							pos={pos}
+							previewShiftInfo={previewShiftInfo}
 							changeOrdering={changeOrdering}
-							isHovered={
-								previewShiftInfo.range && 
-								pos >= previewShiftInfo.range[0] && 
-								pos <= previewShiftInfo.range[1]
-							}
 							dragHandler={dragHandler}
+							key={name}
 						/>
 					)
 				}
@@ -158,7 +204,7 @@ const DraggableTable = ({data}) => {
 						>
 							{
 								columnOrder.map(
-									name => <td style={cellStyle} key={name}>
+									(name, pos) => <td style={getCellStyle(pos === rangeEndpoint)} key={name}>
 										{toCellDisplay(row[name])}
 									</td>
 								)
