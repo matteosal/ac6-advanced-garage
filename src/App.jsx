0e3dd5fe2faf22bb33304d961fdfb738e64501eb
Matteo Salvarezza <matteos@wolfram.com>
2025-03-21 17:22:48 +0100

    Patch 1.8.1 data update

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const SwitchedComponent = ({selectedSwitch}) => {
 	}
 }
 
-const gameVersion = '1.7.2';
+const gameVersion = '1.8.1';
 
 function App() {
 	const [selectedSwitch, setSelectedSwitch] = useState('BUILD');
